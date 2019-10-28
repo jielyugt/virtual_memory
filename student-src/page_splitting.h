143edfd49466ae4f9316a495bf76cbf8a91124c3
jielyugt <jie.lyu@gatech.edu>
2019-10-28 13:31:15 -0400

    vaddr_vpn() and vaddr_offset() implemented

--- student-src/page_splitting.h
+++ student-src/page_splitting.h
@@ -23,12 +23,12 @@
 
 /* Get the virtual page number from a virtual address. */
 static inline vpn_t vaddr_vpn(vaddr_t addr) {
-    return 0;                   /* FIXME */
+    return addr / PAGE_SIZE;
 }
 
 /* Get the offset into the page from a virtual address. */
 static inline uint16_t vaddr_offset(vaddr_t addr) {
-    return 0;                   /* FIXME */
+    return addr % PAGE_SIZE;
 }
 
 #pragma GCC diagnostic pop
